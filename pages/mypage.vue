0c849586ebab0bd85a719da97c567169f6f2ddc0
shinbunbun <takamuneyuto@gmail.com>
2020-04-29 18:39:52 +0900

    fix mypage

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -22,10 +22,10 @@
           </v-card-title>
           <v-card-text>
             <div class="text--primary">
-              <p class="card-text" v-text="memberStatus"></p>
+              <p class="card-text" v-text="memberStatus" />
               <p>
                 会員ID：
-                <span v-text="userId"></span>
+                <span v-text="userId" />
               </p>
             </div>
           </v-card-text>
@@ -37,7 +37,7 @@
 </template>
 
 <script>
-
+/* eslint-disable no-undef */
 export default {
   components: {
   },
@@ -52,6 +52,59 @@ export default {
       postalCode: '',
       jikanwariNotify: ''
     }
+  },
+  mounted() {
+    liff.getProfile()
+      .then(async (profile) => {
+        this.displayName = profile.displayName
+        this.userId = profile.userId
+        this.pictureUrl = profile.pictureUrl
+
+        const userId = profile.userId
+
+        const main = async () => {
+          try {
+            const res = await axios.request({
+              method: 'GET',
+              baseURL: `https://cshm50yn8b.execute-api.ap-northeast-1.amazonaws.com/prod/?userId=${userId}`
+            })
+            return res.data
+          } catch (error) {
+            alert('エラーが発生しました')
+            return error.response.data
+          }
+        }
+        const res = (await main()).ttdata
+        console.log(res)
+        if (res.ttdata === 'no') {
+          alert('時間割登録を完了させて下さい')
+        } else if (res.premium) {
+          this.memberStatus = 'プレミアム会員'
+          this.premiumPeriod = res.premiumPeriod
+
+          if (res.train) {
+            this.trainName = res.train
+          } else {
+            this.trainName = '路線未登録'
+          }
+
+          if (res.postalCode) {
+            this.postalCode = res.postalCode
+          } else {
+            this.postalCode = '地域未登録'
+          }
+
+          if (res.flag && res.flag !== '25') {
+            this.jikanwariNotify = `${res.flag}:00`
+          } else {
+            this.jikanwariNotify = '時間割お知らせ機能未登録'
+          }
+        } else {
+          this.memberStatus = '一般会員'
+        }
+
+        this.isLoading = false
+      })
   }
 }
 </script>
