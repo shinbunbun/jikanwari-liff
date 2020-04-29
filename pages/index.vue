0c815c3ce596b5b75575902aa90abc8e3c9066a9
shinbunbun <takamuneyuto@gmail.com>
2020-04-29 22:29:23 +0900

    debug

--- pages/index.vue
+++ pages/index.vue
@@ -63,6 +63,118 @@ export default {
         if (!liff.isLoggedIn()) {
           // eslint-disable-next-line no-undef
           liff.login();
+        } else {
+          // eslint-disable-next-line no-undef
+          liff.getProfile().then(async(profile) => {
+            const userId = profile.userId;
+            this.userId = userId;
+            let ttdata;
+
+            const session = sessionStorage.getItem('jikanwari');
+            console.log(session);
+
+            if (sessionStorage.getItem('jikanwari')) {
+              console.log(0);
+              ttdata = JSON.parse(sessionStorage.getItem('jikanwari'));
+            } else {
+              console.log(1);
+              const GetTtdataAsync = async() => {
+                let res;
+                try {
+                  const getTtdata = await fetch(`https://cshm50yn8b.execute-api.ap-northeast-1.amazonaws.com/prod?userId=${userId}`, {
+                    mode: 'cors'
+                  });
+                  res = await getTtdata.json();
+                } catch (e) {
+                  // alert
+                  console.log(e);
+                }
+                return res;
+              };
+              const data = await GetTtdataAsync();
+              console.log(data);
+              ttdata = data.ttdata;
+              sessionStorage.setItem('jikanwari', JSON.stringify(ttdata));
+            }
+            console.dir(ttdata);
+
+            if (ttdata === 'no') {
+              // alert
+              console.log('時間割が登録されていません。時間割登録フォームへ移動します。');
+              this.$router.go('/registration');
+            }
+            let sendTt;
+
+            const date = new Date();
+            const dayOfWeek = date.getDay();
+            const dayOfWeekStr = ['日', '月', '火', '水', '木', '金', '土'][dayOfWeek];
+
+            if (ttdata.property) {
+              let property = ttdata.property;
+              property = property.split(',');
+              switch (dayOfWeekStr) {
+                case '日':
+                  sendTt = '今日の時間割はありません';
+                  property = undefined;
+                  break;
+                case '土':
+                  sendTt = ttdata.sat;
+                  property = property[5];
+                  break;
+                case '月':
+                  sendTt = ttdata.mon;
+                  property = property[0];
+                  break;
+                case '火':
+                  sendTt = ttdata.tue;
+                  property = property[1];
+                  break;
+                case '水':
+                  sendTt = ttdata.wed;
+                  property = property[2];
+                  break;
+                case '木':
+                  sendTt = ttdata.thu;
+                  property = property[3];
+                  break;
+                case '金':
+                  sendTt = ttdata.fri;
+                  property = property[4];
+                  break;
+              }
+              console.log(sendTt);
+              this.jikanwari = sendTt;
+              this.property = property;
+            } else {
+              switch (dayOfWeekStr) {
+                case '日':
+                  sendTt = '今日の時間割はありません';
+                  break;
+                case '土':
+                  sendTt = ttdata.sat;
+                  break;
+                case '月':
+                  sendTt = ttdata.mon;
+                  break;
+                case '火':
+                  sendTt = ttdata.tue;
+                  break;
+                case '水':
+                  sendTt = ttdata.wed;
+                  break;
+                case '木':
+                  sendTt = ttdata.thu;
+                  break;
+                case '金':
+                  sendTt = ttdata.fri;
+                  break;
+              }
+              console.log(sendTt);
+              this.jikanwari = sendTt;
+            }
+          }).catch((err) => {
+            console.log('error', err);
+          });
         }
       })
       .catch((err) => {
@@ -70,120 +182,6 @@ export default {
         console.log(err.code, err.message);
         alert('エラーが発生しました');
       });
-    // eslint-disable-next-line no-undef
-    liff.ready.then(() => {
-      // eslint-disable-next-line no-undef
-      liff.getProfile().then(async(profile) => {
-        const userId = profile.userId;
-        this.userId = userId;
-        let ttdata;
-
-        const session = sessionStorage.getItem('jikanwari');
-        console.log(session);
-
-        if (sessionStorage.getItem('jikanwari')) {
-          console.log(0);
-          ttdata = JSON.parse(sessionStorage.getItem('jikanwari'));
-        } else {
-          console.log(1);
-          const GetTtdataAsync = async() => {
-            let res;
-            try {
-              const getTtdata = await fetch(`https://cshm50yn8b.execute-api.ap-northeast-1.amazonaws.com/prod?userId=${userId}`, {
-                mode: 'cors'
-              });
-              res = await getTtdata.json();
-            } catch (e) {
-              // alert
-              console.log(e);
-            }
-            return res;
-          };
-          const data = await GetTtdataAsync();
-          console.log(data);
-          ttdata = data.ttdata;
-          sessionStorage.setItem('jikanwari', JSON.stringify(ttdata));
-        }
-        console.dir(ttdata);
-
-        if (ttdata === 'no') {
-        // alert
-          console.log('時間割が登録されていません。時間割登録フォームへ移動します。');
-          this.$router.go('/registration');
-        }
-        let sendTt;
-
-        const date = new Date();
-        const dayOfWeek = date.getDay();
-        const dayOfWeekStr = ['日', '月', '火', '水', '木', '金', '土'][dayOfWeek];
-
-        if (ttdata.property) {
-          let property = ttdata.property;
-          property = property.split(',');
-          switch (dayOfWeekStr) {
-            case '日':
-              sendTt = '今日の時間割はありません';
-              property = undefined;
-              break;
-            case '土':
-              sendTt = ttdata.sat;
-              property = property[5];
-              break;
-            case '月':
-              sendTt = ttdata.mon;
-              property = property[0];
-              break;
-            case '火':
-              sendTt = ttdata.tue;
-              property = property[1];
-              break;
-            case '水':
-              sendTt = ttdata.wed;
-              property = property[2];
-              break;
-            case '木':
-              sendTt = ttdata.thu;
-              property = property[3];
-              break;
-            case '金':
-              sendTt = ttdata.fri;
-              property = property[4];
-              break;
-          }
-          console.log(sendTt);
-          this.jikanwari = sendTt;
-          this.property = property;
-        } else {
-          switch (dayOfWeekStr) {
-            case '日':
-              sendTt = '今日の時間割はありません';
-              break;
-            case '土':
-              sendTt = ttdata.sat;
-              break;
-            case '月':
-              sendTt = ttdata.mon;
-              break;
-            case '火':
-              sendTt = ttdata.tue;
-              break;
-            case '水':
-              sendTt = ttdata.wed;
-              break;
-            case '木':
-              sendTt = ttdata.thu;
-              break;
-            case '金':
-              sendTt = ttdata.fri;
-              break;
-          }
-          console.log(sendTt);
-          this.jikanwari = sendTt;
-        }
-      }).catch((err) => {
-        console.log('error', err);
-      });
-    });
   }
 };
 </script>
