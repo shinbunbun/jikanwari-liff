a8c87d53f4b7dba508eb77c21f3d0447de1a389b
shinbunbun <takamuneyuto@gmail.com>
2020-04-30 16:44:28 +0900

    fix nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,9 +110,9 @@ export default {
   server: {
     port: 3000,
     host: 'localhost',
-    https: {
+    https: process.env.NODE_ENV !== 'production' ? {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
-    }
+    } : {}
   }
 };
