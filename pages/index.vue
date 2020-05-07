1612d5c761c0546b050f9b5f437eace14f0b3b85
shinbunbun <takamuneyuto@gmail.com>
2020-05-07 13:46:16 +0900

    fix shareTargetPicker

--- pages/index.vue
+++ pages/index.vue
@@ -202,21 +202,21 @@ export default {
   methods: {
     send() {
       // eslint-disable-next-line no-undef
-      if (liff.isApiAvailable('shareTargetPicker')) {
-        // eslint-disable-next-line no-undef
-        liff.shareTargetPicker([
-          {
-            type: 'text',
-            text: this.jikanwari
-          }
-        ])
-          .then(
-          ).catch(function(res) {
-            alert('メッセージを送信できませんでした');
-          });
-      } else {
+      // if (liff.isApiAvailable('shareTargetPicker')) {
+      // eslint-disable-next-line no-undef
+      liff.shareTargetPicker([
+        {
+          type: 'text',
+          text: this.jikanwari
+        }
+      ])
+        .then(
+        ).catch(function(res) {
+          alert('メッセージを送信できませんでした');
+        });
+      /* } else {
         alert('このブラウザはメッセージの送信に非対応です。');
-      }
+      } */
     }
   }
 };
