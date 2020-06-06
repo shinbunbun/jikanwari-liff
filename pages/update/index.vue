b10f1d550ce8545f45284f067728735119898312
shinbunbun <takamuneyuto@gmail.com>
2020-06-07 01:08:11 +0900

    add /update

--- pages/update/index.vue
+++ pages/update/index.vue
@@ -2,29 +2,42 @@
   <v-container column justify-center align-center>
     <v-row class="text-center">
       <v-col>
-        <p style="font-size: 30px">登録完了</p>
+        <p style="font-size: 30px">アップデート</p>
       </v-col>
     </v-row>
-    <v-row class="text-center">
-      <v-col>
-        <p>時間割登録が完了しました！</p>
+    <v-row class="text-right">
+      <v-col />
+      <v-col cols="12" sm="6">
+        <p>更新日時: 2020/6/7</p>
       </v-col>
+      <v-col />
     </v-row>
     <v-row>
-      <v-col class="text-center">
-        <v-btn nuxt to="/" color="primary" x-large width="140px">ホームに戻る</v-btn>
+      <v-col />
+      <v-col cols="12" sm="6">
+        <h2>v2.0.0</h2>
       </v-col>
-      <v-col>
-        <v-btn
-          href="https://line.me/R/ti/p/%40ywg0561x"
-          target="_blank"
-          color="#00b900"
-          x-large
-          width="140px"
-        >
-          <font color="white">LINEに戻る</font>
-        </v-btn>
+      <v-col />
+    </v-row>
+    <v-row>
+      <v-col />
+      <v-col cols="12" sm="6">
+        <ul>
+          <li>時間割共有で持ち物が共有されないバグを修正しました</li>
+          <li>時間割編集をすると共有IDが変わってしまうバグを修正しました</li>
+          <li>メニューを一部修正しました</li>
+          <li>バージョンを発行することにしました</li>
+          <li>時間割お知らせ機能が復活しました</li>
+        </ul>
       </v-col>
+      <v-col />
+    </v-row>
+    <v-row class="text-right">
+      <v-col />
+      <v-col cols="12" sm="6">
+        <p>更新日時: 2020/6/7</p>
+      </v-col>
+      <v-col />
     </v-row>
   </v-container>
 </template>
